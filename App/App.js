3b6ff1bd6d44f6d02b6dc9a76124414136557209
Abhay Zala <zala.abhay@gmail.com>
2021-03-23 03:51:49 -0400

    Element creation

--- App/App.js
+++ App/App.js
@@ -2,6 +2,24 @@ class jHex {
 	constructor() {
 		$("body").css({"margin" : "0"});
 		$("body").maximize();
+
+		this.root = $("#root");
+	}
+
+	createElement(tag, attributes) {
+		let base = $(document.createElement(tag));
+
+		if (attributes) {
+			for (let attr in attributes) {
+				base.attr(attr, attributes[attr]);
+			}
+		}
+
+		this.root.append(base);
+	}
+
+	defineType() {
+
 	}
 }
 
@@ -16,17 +34,45 @@ class HexObject {
 
 	css(properties) {
 		for (let key in properties) {
-			this.element.style[key] = properties[key];
+			this.root.style[key] = properties[key];
 		}
+	}
+
+	attr(attr, value) {
+		this.root.setAttribute(attr, value);
 	}
 
 	maximize() {
 		this.css({"width" : "100%", "height" : "100%"});
 	}
+
+	append(element) {
+		if (typeof content == HexObject) {
+			this.root.appendChild(element.root);
+		} else if (typeof content == 'object') {
+			this.root.appendChild(element);
+		}
+	}
+
+	content(content) {
+		if (typeof content == HexObject) {
+			this.append(content.root);
+		} else if (typeof content == 'object') {
+			this.append(content);
+		} else if (typeof content == 'string') {
+			this.root.innerHTML = content;
+		}
+	}
 }
 
 function $(selector) {
-	return new HexObject(document.querySelector(selector));
+	if (typeof selector == "string") {
+		return new HexObject(document.querySelector(selector));
+	} else if (typeof selector == "object") {
+		return new HexObject(selector);
+	} else if (typeof selector == 'function') {
+		selector();
+	}
 }
 
 
---
+++ demo.js
@@ -0,0 +1,3 @@
+$(function() {
+	App.createElement("div", {"type" : "button"});
+});
